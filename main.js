0bfbbd02fad8d294f144bb04c0dcd58904bb4892
emmabennett <emma.j.bennett15@gmail.com>
2018-06-06 21:03:03 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#sec-1").click(function(){
+        $("#p1").toggle();
+    });
+
+    $("#sec-2").click(function(){
+        $("#p2").toggle();
+    });
+
+    $("#sec-3").click(function(){
+        $("#p3").toggle();
+    });
+
+    $("#sec-4").click(function(){
+        $("#p4").toggle();
+    });
+
+    $("#sec-5").click(function(){
+        $("#p5").toggle();
+    });
+ 
+});
